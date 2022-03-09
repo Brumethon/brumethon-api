3ef4ed7c20fcc2693dbdf4f294866ac93447a75d
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-09 21:05:47 +0100

    init test

---
+++ src/test/java/com/brumethon/TestTest.java
@@ -0,0 +1,13 @@
+package com.brumethon;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+
+    @Test
+    void should_be_4(){
+        Assertions.assertEquals(4, 2 + 2);
+    }
+
+}

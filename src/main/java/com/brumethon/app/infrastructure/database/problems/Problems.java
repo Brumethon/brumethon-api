b9c90dcf95c2be2866212d3cf23258824fcacafc
Quentin MOLERO <quentin.molero@outlook.com>
2022-03-10 12:04:06 +0100

    Refactored Problems table

--- src/main/java/com/brumethon/app/infrastructure/database/problems/Problems.java
+++ src/main/java/com/brumethon/app/infrastructure/database/problems/Problems.java
@@ -1,5 +1,6 @@
 package com.brumethon.app.infrastructure.database.problems;
 
+import com.brumethon.app.infrastructure.database.referent.Referent;
 import com.brumethon.app.infrastructure.database.scooter.Scooter;
 
 import javax.persistence.*;
@@ -19,19 +20,22 @@ public class Problems {
     private ProblemStatus status;
     private String name;
     private String description;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Referent referent;
     private Double latitude;
     private Double longitude;
 
     public Problems() {
     }
 
-    public Problems(LocalDate date, Scooter scooter, Categories categories, ProblemStatus status, String name, String description, Double latitude, Double longitude) {
+    public Problems(LocalDate date, Scooter scooter, Categories categories, ProblemStatus status, String name, String description, Referent referent, Double latitude, Double longitude) {
         this.date = date;
         this.scooter = scooter;
         this.categories = categories;
         this.status = status;
         this.name = name;
         this.description = description;
+        this.referent = referent;
         this.latitude = latitude;
         this.longitude = longitude;
     }

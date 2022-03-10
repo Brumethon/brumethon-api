9d056c942601c4f1364a6a3ce310e7d668182d0c
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-10 19:08:20 +0100

    updated problem status DB repository

--- src/main/java/com/brumethon/app/infrastructure/database/problemstatus/ProblemStatusDBRepository.java
+++ src/main/java/com/brumethon/app/infrastructure/database/problemstatus/ProblemStatusDBRepository.java
@@ -2,5 +2,5 @@ package com.brumethon.app.infrastructure.database.problemstatus;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProblemStatusDBRepository extends CrudRepository<ProblemStatusDB, String> {
+public interface ProblemStatusDBRepository extends CrudRepository<ProblemStatusDB, Long> {
 }

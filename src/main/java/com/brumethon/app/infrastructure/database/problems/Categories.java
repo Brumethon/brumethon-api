6bf0e2925af78b7649153a8fa5019e2eedc46551
Quentin MOLERO <quentin.molero@outlook.com>
2022-03-10 11:56:02 +0100

    Refactored Categories and Referent tables

--- src/main/java/com/brumethon/app/infrastructure/database/problems/Categories.java
+++ src/main/java/com/brumethon/app/infrastructure/database/problems/Categories.java
@@ -1,12 +1,18 @@
 package com.brumethon.app.infrastructure.database.problems;
 
+import com.brumethon.app.infrastructure.database.referent.Referent;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
 
 @Entity
 public class Categories {
     @Id
     private String name;
+    @ManyToMany
+    private Set<Referent> referents;
 
     public Categories() {
     }

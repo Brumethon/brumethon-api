4c62a8f1def6eeda9fc4f00f29b85a8f76ee3095
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-10 19:08:20 +0100

    updated problem status DB repository

--- src/main/java/com/brumethon/app/infrastructure/database/problemstatus/ProblemStatusDB.java
+++ src/main/java/com/brumethon/app/infrastructure/database/problemstatus/ProblemStatusDB.java
@@ -1,5 +1,7 @@
 package com.brumethon.app.infrastructure.database.problemstatus;
 
+import com.brumethon.app.domain.problemestatus.ProblemStatus;
+
 import javax.persistence.*;
 
 @Table(name = "problem_status")
@@ -13,5 +15,26 @@ public class ProblemStatusDB {
     private String name;
 
     public ProblemStatusDB() {
+    }
+
+    public ProblemStatusDB(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static ProblemStatusDB of(ProblemStatus problemStatus){
+        return new ProblemStatusDB(problemStatus.getID(), problemStatus.getName());
+    }
+
+    public ProblemStatus toProblemStatus(){
+        return new ProblemStatus(id, name);
     }
 }
--- src/main/java/com/brumethon/app/infrastructure/repository/InDBProblemStatusRepository.java
+++ src/main/java/com/brumethon/app/infrastructure/repository/InDBProblemStatusRepository.java
@@ -2,19 +2,37 @@ package com.brumethon.app.infrastructure.repository;
 
 import com.brumethon.app.domain.problemestatus.ProblemStatus;
 import com.brumethon.app.domain.problemestatus.ProblemStatusRepository;
+import com.brumethon.app.infrastructure.database.problemstatus.ProblemStatusDB;
+import com.brumethon.app.infrastructure.database.problemstatus.ProblemStatusDBRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class InDBProblemStatusRepository implements ProblemStatusRepository {
+
+    private final ProblemStatusDBRepository dbRepository;
+
+    public InDBProblemStatusRepository(ProblemStatusDBRepository dbRepository) {
+        this.dbRepository = dbRepository;
+    }
+
     @Override
     public Optional<ProblemStatus> get(Long key) {
-        return Optional.empty();
+        Optional<ProblemStatusDB> problemStatusDB  = dbRepository.findById(key);
+
+        if(problemStatusDB.isEmpty()){
+            return Optional.empty();
+        }
+
+        return Optional.of( problemStatusDB.get().toProblemStatus() );
     }
 
     @Override
     public Long add(ProblemStatus value) {
-        return null;
+        ProblemStatusDB problemStatusDB = dbRepository.save(ProblemStatusDB.of(value));
+        value.setId(problemStatusDB.getId());
+        return problemStatusDB.getId();
     }
 
     @Override
@@ -24,11 +42,14 @@ public class InDBProblemStatusRepository implements ProblemStatusRepository {
 
     @Override
     public boolean remove(Long value) {
-        return false;
+        dbRepository.deleteById(value);
+        return dbRepository.existsById(value);
     }
 
     @Override
     public List<ProblemStatus> getAll() {
-        return null;
+        List<ProblemStatus> list = new ArrayList<>();
+        dbRepository.findAll().forEach(problemStatusDB -> list.add(problemStatusDB.toProblemStatus()));
+        return list;
     }
 }

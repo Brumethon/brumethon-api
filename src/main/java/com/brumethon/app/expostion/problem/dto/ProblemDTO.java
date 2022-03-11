2cdcce57c6c9b4e03c3dae908e9caadb69405c2a
Quentin MOLERO <quentin.molero@outlook.com>
2022-03-11 09:55:48 +0100

    Fix constructor for ProblemDTO

--- src/main/java/com/brumethon/app/expostion/problem/dto/ProblemDTO.java
+++ src/main/java/com/brumethon/app/expostion/problem/dto/ProblemDTO.java
@@ -22,7 +22,7 @@ public class ProblemDTO {
     public ProblemDTO() {
     }
 
-    public ProblemDTO(Long id, String name, String description, ScooterDTO scooterDTO, Double latitude, Double longitude, LocalDate date, CategoryDTO categoryDTO) {
+    public ProblemDTO(Long id, String name, String description, ScooterDTO scooterDTO, Double latitude, Double longitude, LocalDate date, UserDTO owner, UserDTO referent, CategoryDTO categoryDTO) {
         this.id = id;
         this.name = name;
         this.description = description;

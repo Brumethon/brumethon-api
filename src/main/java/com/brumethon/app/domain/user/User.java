9c5813a68b21597ce7b069c2ff0ee80d59ad26fe
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-11 10:48:20 +0100

    probleme distance filter

--- src/main/java/com/brumethon/app/domain/user/User.java
+++ src/main/java/com/brumethon/app/domain/user/User.java
@@ -50,7 +50,33 @@ public class User extends Entity<Long> {
         this(null, firstName, lastName, password, phoneNumber, emailAddress, address, new ArrayList<>(), new ArrayList<>());
     }
 
+    private double meterDistanceBetweenPoints(double lat_a, double lng_a, double lat_b, double lng_b) {
+        float pk = (float) (180.f/Math.PI);
+
+        double a1 = lat_a / pk;
+        double a2 = lng_a / pk;
+        double b1 = lat_b / pk;
+        double b2 = lng_b / pk;
+
+        double t1 = Math.cos(a1) * Math.cos(a2) * Math.cos(b1) * Math.cos(b2);
+        double t2 = Math.cos(a1) * Math.sin(a2) * Math.cos(b1) * Math.sin(b2);
+        double t3 = Math.sin(a1) * Math.sin(b1);
+        double tt = Math.acos(t1 + t2 + t3);
+
+        return 6366000 * tt;
+    }
+
     public boolean isUserAvailableForProblem(Problem problem){
+
+        if( problem.getReferent() != null){
+            return false;
+        }
+
+        if(meterDistanceBetweenPoints(problem.getCoordinate().getLatitude(), problem.getCoordinate().getLongitude(),
+                this.address.getLatitude(), this.address.getLongitude()) >= 10000){
+            return false;
+        }
+
         return true;
     }
 

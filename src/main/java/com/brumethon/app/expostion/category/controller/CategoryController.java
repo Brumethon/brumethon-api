e9733b9f3c9e295d6a4bae9dcd5c27a4b3a323a8
paula <paulabib17@gmail.com>
2022-03-11 08:50:15 +0100

    create category

--- src/main/java/com/brumethon/app/expostion/category/controller/CategoryController.java
+++ src/main/java/com/brumethon/app/expostion/category/controller/CategoryController.java
@@ -1,12 +1,12 @@
 package com.brumethon.app.expostion.category.controller;
 
+import com.brumethon.app.domain.categories.Categories;
 import com.brumethon.app.expostion.category.dto.CategoryDTO;
+import com.brumethon.app.expostion.category.dto.CreateCategoryDTO;
 import com.brumethon.app.expostion.error.ErrorHandler;
 import com.brumethon.app.expostion.user.dto.UserDTO;
 import com.brumethon.app.infrastructure.service.CategoriesService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -33,5 +33,10 @@ public class CategoryController extends ErrorHandler {
         return categoriesService.getAllCategoriesUsers(id).stream()
                 .map(user -> new UserDTO( user.getEmailAddress().toString(), user.getLastName(), user.getFirstName(), user.getAddress().toString()))
                 .collect(Collectors.toList());
+    }
+
+    @PostMapping(value = "/categories")
+    public void postCategory(@RequestBody @Valid CreateCategoryDTO createCategoryDTO) {
+        categoriesService.add(new Categories(-1L,createCategoryDTO.name));
     }
 }

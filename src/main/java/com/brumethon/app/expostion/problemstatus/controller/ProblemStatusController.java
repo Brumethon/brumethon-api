3677c9424950f30ac17404a9a2bc48a6c8d402e1
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-11 02:19:30 +0100

    updated ProblemStatus to use services

--- src/main/java/com/brumethon/app/expostion/problemstatus/controller/ProblemStatusController.java
+++ src/main/java/com/brumethon/app/expostion/problemstatus/controller/ProblemStatusController.java
@@ -2,8 +2,7 @@ package com.brumethon.app.expostion.problemstatus.controller;
 
 import com.brumethon.app.domain.problemestatus.ProblemStatus;
 import com.brumethon.app.expostion.problemstatus.dto.ProblemStatusDTO;
-import com.brumethon.app.infrastructure.repository.InDBProblemStatusRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.brumethon.app.infrastructure.service.ProblemStatusService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,22 +14,21 @@ import java.util.stream.Collectors;
 @RestController
 public class ProblemStatusController {
 
-    @Autowired
-    private final InDBProblemStatusRepository inDBProblemStatusRepository;
+    private final ProblemStatusService problemStatusService;
 
-    public ProblemStatusController(InDBProblemStatusRepository inDBProblemStatusRepository) {
-        this.inDBProblemStatusRepository = inDBProblemStatusRepository;
+    public ProblemStatusController(ProblemStatusService problemStatusService) {
+        this.problemStatusService = problemStatusService;
     }
 
     @GetMapping(value = "problemstatus")
     public List<ProblemStatusDTO> getProblemStatus() {
-        return inDBProblemStatusRepository.getAll().stream()
+        return problemStatusService.getAll().stream()
                 .map(problemStatus -> new ProblemStatusDTO(problemStatus.getName())).collect(Collectors.toList());
     }
 
     @GetMapping(value = "problemstatus/{status}")
     public ProblemStatusDTO getProblemStatusByStatus(@PathVariable @Valid String status) {
-        ProblemStatus problemStatus = inDBProblemStatusRepository.getByName(status);
+        ProblemStatus problemStatus = problemStatusService.getByName(status);
         return new ProblemStatusDTO(problemStatus.getName());
     }
 }

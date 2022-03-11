6770a31c85653cc5cceb12204e1540fdcb2a7a62
Chottot <71697340+Chottot@users.noreply.github.com>
2022-03-11 01:32:38 +0100

    updated User-categories controller

---
+++ src/main/java/com/brumethon/app/expostion/referentcategorie/controller/ReferentCategoriesController.java
@@ -0,0 +1,9 @@
+package com.brumethon.app.expostion.referentcategorie.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReferentCategoriesController {
+
+
+}
